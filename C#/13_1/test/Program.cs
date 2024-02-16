2566428f7f52591c24582f36df5be3d552c9cbdb
이승준 <37408775+Paladin-Danse@users.noreply.github.com>
2024-02-16 20:28:08 +0900

    깃무시파일 목록추가, 유니티 심화 강의 프로젝트 생성, 코드카타 테스트

--- C#/13_1/test/Program.cs
+++ C#/13_1/test/Program.cs
@@ -6,26 +6,39 @@
         {
             Solution solution = new Solution();
 
-            int i = solution.solution("one4seveneight");
+            int[] i = solution.solution(3, score: [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]);
 
-            Console.WriteLine(i);
+            for(int j=0; j<i.Length; j++)
+                Console.Write(i[j] + " ");
         }
     }
     public class Solution
     {
-        public int solution(string s)
+        public int[] solution(int k, int[] score)
         {
-            int answer = 0;
-            string[] numString = new string[10] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-
-            while (!int.TryParse(s, out answer))
+            int[] answer = new int[score.Length];
+            int[] HallofFame = new int[k];
+            for (int i = 0; i < score.Length; i++)
             {
-                for (int i = 0; i < numString.Length; i++)
+                if (i >= k)
                 {
-                    s = s.Replace(numString[i], i.ToString());
+                    int minScore = HallofFame.Min();
+                    if (minScore < score[i])
+                    {
+                        HallofFame[Array.IndexOf(HallofFame, minScore)] = score[i];
+                        minScore = HallofFame.Min();
+                    }
+                    answer[i] = minScore;
+                }
+                else
+                {
+                    HallofFame[i] = score[i];
+                    answer[i] = HallofFame.Where(x => x != 0).Min();
                 }
             }
 
+            for (int i = 0; i < HallofFame.Length; i++)
+                Console.WriteLine(HallofFame[i]);
             return answer;
         }
     }
