83a7ec2f779f49dfae51c96620233745259e798d
이승준 <37408775+Paladin-Danse@users.noreply.github.com>
2024-02-19 21:44:47 +0900

    유니티 심화과정 강의 - 플레이어 조작

---
+++ UnityStudy/My project/Assets/Scripts/Characters/Player/StateMachine/PlayerBaseState.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PlayerBaseState : IState
+{
+    protected PlayerStateMachine stateMachine;
+    protected readonly PlayerGroundData groundData;
+
+    public PlayerBaseState(PlayerStateMachine playerStateMachine)
+    {
+        stateMachine = playerStateMachine;
+        groundData = stateMachine.player.Data.GroundData;
+    }
+
+    public virtual void Enter()
+    {
+        AddInputActionsCallbacks();
+    }
+
+    public virtual void Exit()
+    {
+        RemoveInputActionsCallbacks();
+    }
+
+    public virtual void HandleInput()
+    {
+        ReadMovementInput();
+    }
+
+    public virtual void Update()
+    {
+        Move();
+    }
+    public virtual void PhysicsUpdate()
+    {
+        
+    }
+
+    protected virtual void AddInputActionsCallbacks()
+    {
+        PlayerInput input = stateMachine.player.Input;
+        input.PlayerActions.Movement.canceled += OnMovementCanceled;
+        input.PlayerActions.Run.started += OnRunStarted;
+
+        stateMachine.player.Input.PlayerActions.Jump.started += OnJumpStarted;
+    }
+    protected virtual void RemoveInputActionsCallbacks()
+    {
+        PlayerInput input = stateMachine.player.Input;
+        input.PlayerActions.Movement.canceled -= OnMovementCanceled;
+        input.PlayerActions.Run.started -= OnRunStarted;
+
+        stateMachine.player.Input.PlayerActions.Jump.started -= OnJumpStarted;
+    }
+
+    protected virtual void OnRunStarted(InputAction.CallbackContext context)
+    {
+
+    }
+    protected virtual void OnMovementCanceled(InputAction.CallbackContext context)
+    {
+
+    }
+
+    protected virtual void OnJumpStarted(InputAction.CallbackContext context)
+    {
+
+    }
+
+    private void ReadMovementInput()
+    {
+        stateMachine.MovementInput = stateMachine.player.Input.PlayerActions.Movement.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        Vector3 movementDirection = GetMovementDirection();
+
+        Rotate(movementDirection);
+
+        Move(movementDirection);
+    }
+
+    private Vector3 GetMovementDirection()
+    {
+        Vector3 forward = stateMachine.MainCameraTransform.forward;
+        Vector3 right = stateMachine.MainCameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        return forward * stateMachine.MovementInput.y + right * stateMachine.MovementInput.x;
+    }
+    private void Rotate(Vector3 movementDirection)
+    {
+        if(movementDirection != Vector3.zero)
+        {
+            Transform playerTransform = stateMachine.player.transform;
+            Quaternion targetRotation = Quaternion.LookRotation(movementDirection);
+            stateMachine.player.transform.rotation = Quaternion.Slerp(playerTransform.rotation, targetRotation, stateMachine.RotationDamping * Time.deltaTime);
+        }
+    }
+
+    private void Move(Vector3 movementDirection)
+    {
+        float movementSpeed = GetMovementSpeed();
+        stateMachine.player.Controller.Move(
+            ((movementDirection * movementSpeed) +
+            stateMachine.player.ForceReceiver.Movement) *
+            Time.deltaTime
+            );
+    }
+
+    private float GetMovementSpeed()
+    {
+        float movementSpeed = stateMachine.MovementSpeed * stateMachine.MovementSpeedModifier;
+        return movementSpeed;
+    }
+    protected void StartAnimation(int animationHash)
+    {
+        stateMachine.player.Animator.SetBool(animationHash, true);
+    }
+
+    protected void StopAnimation(int animationHash)
+    {
+        stateMachine.player.Animator.SetBool(animationHash, false);
+    }
+}
+    
---
+++ UnityStudy/My project/Assets/Scripts/Characters/Player/StateMachine/PlayerFallState.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFallState : PlayerAirState
+{
+    public PlayerFallState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        StartAnimation(stateMachine.player.AnimationData.fallParameterHash);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+        StopAnimation(stateMachine.player.AnimationData.fallParameterHash);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if(stateMachine.player.Controller.isGrounded)
+        {
+            stateMachine.ChangeState(stateMachine.IdleState);
+        }
+    }
+}
